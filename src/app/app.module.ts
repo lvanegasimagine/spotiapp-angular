a4c8519ccfd94ebeda7af42bebcf1a4aa4bffcfc
Luis Vanegas <lvanegas1429@gmail.com>
2020-05-28 10:10:23 -0600

    Creacion de proyecto y componentes
    Instalación del Bootstrap y archivos styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { ArtistaComponent } from './pages/artista/artista.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
