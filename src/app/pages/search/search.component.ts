da548b611ae36f73a3a64c277834ce19b624af7c
Luis Vanegas <lvanegas1429@gmail.com>
2020-05-28 15:31:30 -0600

    Componentes de Busqueda de artistas

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[] = [];
+  constructor(private spotify: SpotifyService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(termino: string){
+      console.log(termino);
+      this.spotify.getArtista(termino)
+        .subscribe((data: any) =>{
+          console.log(data.artists.items);
+          this.artistas = data.artists.items;
+        });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,12 +13,19 @@ export class SpotifyService {
 
   getNewRelease(){
 
-    const headers = new HttpHeaders(
-      {
+    const headers = new HttpHeaders({
         'Authorization': 'Bearer BQDjNJKbCSDR5fkxj8N1YzvUInL-l417FeMNUH2m7EMszRv6YOMhws0Nm72DB4HwULjehI7eO9Rz3k8RYsI'
       });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
 
+  }
+
+  getArtista(termino: string){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDjNJKbCSDR5fkxj8N1YzvUInL-l417FeMNUH2m7EMszRv6YOMhws0Nm72DB4HwULjehI7eO9Rz3k8RYsI'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
   }
 }
